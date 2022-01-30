6a1dd57f69792351effd7ca091a0b5c734ae1f3b
Saulo Mairesse <saulodavi2001@gmail.com>
2022-01-29 22:51:15 -0300

    Versão com a função selecionarOpcao e AtivarBotao

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function selecionarOpcao(escolha, section){
     const opcaoSelecionada = document.querySelector(`#${section} .bordaEicone`);
-    if(opcaoSelecionada !== null && opcaoSelecionada !== escolha) opcaoSelecionada.classList.remove('bordaEicone');
+    if(opcaoSelecionada !== null && opcaoSelecionada !== escolha) {
+        opcaoSelecionada.classList.remove('bordaEicone');
+    }
     escolha.classList.toggle('bordaEicone');
+    confirmarPedido();
+}
+
+function confirmarPedido(){    
+    const botao = document.querySelector('footer button');
+
+    prato = document.querySelector('#prato .bordaEicone');
+    bebida = document.querySelector('#bebida .bordaEicone');
+    sobremesa = document.querySelector('#sobremesa .bordaEicone');
+
+    console.log(prato);
+     if(prato != null && bebida != null && sobremesa != null){
+        botao.classList.add('botaoAtivo');    
+    }
+    else botao.classList.remove('botaoAtivo')
 }
