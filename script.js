47293cf5eae0d9699496eeb8d4ea51d4f8e403db
Saulo Mairesse <saulodavi2001@gmail.com>
2022-01-30 15:17:51 -0300

    Arrumei o Botao Finalizar pedido

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
+// DEIXAR COM BORDA
 function selecionarOpcao(escolha, section){
     const opcaoSelecionada = document.querySelector(`#${section} .bordaEicone`);
-    if(opcaoSelecionada !== null && opcaoSelecionada !== escolha) {
+    if(opcaoSelecionada !== null && opcaoSelecionada !== escolha){
         opcaoSelecionada.classList.remove('bordaEicone');
     }
     escolha.classList.toggle('bordaEicone');
     confirmarPedido();
 }
 
+// DEIXAR O BOTAO DE CONFIRMAR VERDE
 function confirmarPedido(){    
-    const botao = document.querySelector('footer button');
+    const finalizarPedido = document.querySelector('footer button');
+    const finalizarTexto = document.querySelector('.concluirPedido');
 
     prato = document.querySelector('#prato .bordaEicone');
     bebida = document.querySelector('#bebida .bordaEicone');
     sobremesa = document.querySelector('#sobremesa .bordaEicone');
 
-    console.log(prato);
      if(prato != null && bebida != null && sobremesa != null){
-        botao.classList.add('botaoAtivo');    
+        finalizarPedido.classList.add('botaoAtivo'); 
+        finalizarTexto.innerHTML='Finalizar Pedido';
     }
-    else botao.classList.remove('botaoAtivo')
+    else finalizarPedido.classList.remove('botaoAtivo')
 }
+
+// COLOCAR ICONE
+// function testeIcone(escolha, section){
+//     const parteIcone = document.querySelector(`#${section} .icone`);
+//     if(parteIcone !== null && parteIcone !== escolha) {
+//         parteIcone.classList.remove('bordaEicone');
+//     }
+// }
