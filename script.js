55402149e9a4c6c70c10ed8fa6ca6af890437086
Saulo Mairesse <saulodavi2001@gmail.com>
2022-01-30 22:08:20 -0300

    Versão s/ icone e sem envio WPP

--- script.js
+++ script.js
@@ -12,22 +12,32 @@ function selecionarOpcao(escolha, section){
 function confirmarPedido(){    
     const finalizarPedido = document.querySelector('footer button');
     const finalizarTexto = document.querySelector('.concluirPedido');
+    const criarlink = document.querySelector('.link');
 
     prato = document.querySelector('#prato .bordaEicone');
     bebida = document.querySelector('#bebida .bordaEicone');
     sobremesa = document.querySelector('#sobremesa .bordaEicone');
 
+    // const pedidoT = document.querySelector('#pratos .bordaEicone .nomePrato');
+    // console.log(pedidoT);
+    // const pratoT = "pratoT";
+    // const sobremesaT = "sobremesaT";
+    // const totalT = "totalT";
+
+    // Problema com formatação do texto
+    // var texto = `Olá, gostaria de fazer o ${pedidoT}:\n- Prato: ${pratoT}\n- Bebida: ${bebidaT}\n- Sobremesa: ${sobremesaT}\nTotal: R$ ${totalT}`;
+    // texto = window.encodeURIComponent(texto);
+
      if(prato != null && bebida != null && sobremesa != null){
         finalizarPedido.classList.add('botaoAtivo'); 
         finalizarTexto.innerHTML='Finalizar Pedido';
+        criarlink.setAttribute("target","_blank");
+        criarlink.setAttribute("href", `https://wa.me/5563999143014?text=Olá,%20gostaria%20de%20fazer%20o%20:`);
     }
-    else finalizarPedido.classList.remove('botaoAtivo')
+    else {
+        finalizarPedido.classList.remove('botaoAtivo')
+        finalizarTexto.innerHTML='Selecione os 3 itens para fechar o pedido';
+        criarlink.removeAttribute("target");
+        criarlink.removeAttribute("href");
+    }
 }
-
-// COLOCAR ICONE
-// function testeIcone(escolha, section){
-//     const parteIcone = document.querySelector(`#${section} .icone`);
-//     if(parteIcone !== null && parteIcone !== escolha) {
-//         parteIcone.classList.remove('bordaEicone');
-//     }
-// }
