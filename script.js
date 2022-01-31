da7d473d8cd4b709138f305920bc284d95ef655b
Saulo Mairesse <saulodavi2001@gmail.com>
2022-01-31 11:34:18 -0300

    Arrumei id=class e margem+icone colocados, faltando envio de msg

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
-// DEIXAR COM BORDA
-function selecionarOpcao(escolha, section){
-    const opcaoSelecionada = document.querySelector(`#${section} .bordaEicone`);
-    if(opcaoSelecionada !== null && opcaoSelecionada !== escolha){
-        opcaoSelecionada.classList.remove('bordaEicone');
+let listaNome = [];
+function selecionarEscolha(escolha,section){
+    const escolhaSelecionada = document.querySelector(`.${section} .bordaEicone`);
+    const icone = document.querySelector(`.${section} .bordaEicone .icone`);
+    console.log(icone)
+    if(escolhaSelecionada !== null && escolhaSelecionada !== escolha){
+        escolhaSelecionada.classList.remove('bordaEicone')
     }
     escolha.classList.toggle('bordaEicone');
-    confirmarPedido();
+    finalizarPedido();
 }
 
-// DEIXAR O BOTAO DE CONFIRMAR VERDE
-function confirmarPedido(){    
+// // DEIXAR COM BORDA
+// function selecionarOpcao(escolha, section){
+//     const opcaoSelecionada = document.querySelector(`#${section} .bordaEicone`);
+//     console.log(opcaoSelecionada);
+//     if(opcaoSelecionada !== null && opcaoSelecionada !== escolha){
+//         opcaoSelecionada.classList.remove('bordaEicone');
+//     }
+//     escolha.classList.toggle('bordaEicone');
+//     confirmarPedido();
+// }
+
+//DEIXAR O BOTAO DE CONFIRMAR VERDE
+function finalizarPedido(){    
     const finalizarPedido = document.querySelector('footer button');
-    const finalizarTexto = document.querySelector('.concluirPedido');
+    const finalizarTexto = document.querySelector('.finalizarPedido');
     const criarlink = document.querySelector('.link');
 
-    prato = document.querySelector('#prato .bordaEicone');
-    bebida = document.querySelector('#bebida .bordaEicone');
-    sobremesa = document.querySelector('#sobremesa .bordaEicone');
+    prato = document.querySelector('.prato .bordaEicone');
+    bebida = document.querySelector('.bebida .bordaEicone');
+    sobremesa = document.querySelector('.sobremesa .bordaEicone');
 
-    // const pedidoT = document.querySelector('#pratos .bordaEicone .nomePrato');
-    // console.log(pedidoT);
-    // const pratoT = "pratoT";
-    // const sobremesaT = "sobremesaT";
-    // const totalT = "totalT";
-
-    // Problema com formatação do texto
-    // var texto = `Olá, gostaria de fazer o ${pedidoT}:\n- Prato: ${pratoT}\n- Bebida: ${bebidaT}\n- Sobremesa: ${sobremesaT}\nTotal: R$ ${totalT}`;
-    // texto = window.encodeURIComponent(texto);
-
-     if(prato != null && bebida != null && sobremesa != null){
+    if(prato != null && bebida != null && sobremesa != null){
         finalizarPedido.classList.add('botaoAtivo'); 
         finalizarTexto.innerHTML='Finalizar Pedido';
         criarlink.setAttribute("target","_blank");
-        criarlink.setAttribute("href", `https://wa.me/5563999143014?text=Olá,%20gostaria%20de%20fazer%20o%20:`);
+        criarlink.setAttribute("href", `https://wa.me/5563999143014?text=PUTAMERDA`);
     }
     else {
         finalizarPedido.classList.remove('botaoAtivo')
         finalizarTexto.innerHTML='Selecione os 3 itens para fechar o pedido';
         criarlink.removeAttribute("target");
-        criarlink.removeAttribute("href");
+        criarlink.removeAttribute("href")
     }
 }
